1c08cc9770101fe358bf9f8eedd730ff72a98732
15144895548 <15144895548@163.com>
2023-05-06 09:43:20 +0800

    commit second hotfix comment

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -5,6 +5,6 @@ public class App {
         System.out.println("Hello Git");
         System.out.println("second");
         System.out.println("third");
-        System.out.println("third 02");
+        System.out.println("third 03");
     }
 }
